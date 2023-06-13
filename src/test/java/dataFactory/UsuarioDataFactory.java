f1824e526592942e4a5a4206f84d1ad9a50f05f2
Deborah <deborahschubert@gmail.com>
2023-06-13 17:53:26 -0300

    atualizei informações

--- src/test/java/dataFactory/UsuarioDataFactory.java
+++ src/test/java/dataFactory/UsuarioDataFactory.java
@@ -5,8 +5,8 @@ import Pojo.UsuarioPojo;
 public class UsuarioDataFactory {
     public static UsuarioPojo criarUsuarioAdministrador(){
         UsuarioPojo usuario = new UsuarioPojo();
-        usuario.setUsuarioLogin("admin");
-        usuario.setUsuarioSenha("admin");
+        usuario.setUsuarioLogin("inserir usuário");
+        usuario.setUsuarioSenha("inserir senha");
 
         return usuario;
     }
